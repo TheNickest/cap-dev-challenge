44bde2ad08d21ffaa938675caff995b539d552e1
TheNickest <53490469+TheNickest@users.noreply.github.com>
2024-02-16 22:03:31 +0000

    add working solution

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const cds = require('@sap/cds')
+const graphql = require('@cap-js/graphql')
 
 cds.on('serving', (service) => {
   if (service.name === 'CatalogService') {
-    console.log(service.endpoints[1]?.path)
-    return service.endpoints[1]?.path
+    const provider = (entity) => {
+      if (entity) return
+      return { href: 'graphql', name: 'GraphQl', title: 'GraphQL' }
+    }
+    service.$linkProviders
+      ? service.$linkProviders.push(provider)
+      : (service.$linkProviders = [provider])
   }
 })
-cds.once('bootstrap', (app) => {
-  app.serve('/graphql').from('@cap-js/graphql', 'app/graphql.html')
-})
+// cds.once('bootstrap', (app) => {
+//   app.serve('/graphql').from('@cap-js/graphql', 'app/graphql.html')
+// })
+
+module.exports = cds.server
